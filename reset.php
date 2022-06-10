79911a6cb25dc5698119a7c69df04b103237557d
erfanscode <96702228+erfanscode@users.noreply.github.com>
2022-06-10 15:21:08 +0430

    write code reset password

--- reset.php
+++ reset.php
@@ -1,9 +1,10 @@
-<?php include "vendors/db.php"; ?>
-<?php include "vendors/header.php"; ?>
-<?php ch_title("بازیابی"); ?>
+<?php ob_start(); ?>
+<?php include "vendors/db.php" ?>
+<?php include "vendors/header.php" ?>
+<?php ch_title("بازیابی") ?>
 <?php
-if(!isset($_GET['email'])) {
-    redirect('index.php');
+if(!isset($_GET['email']) && !isset($_GET['token']))    {
+    header("Location: index.php");
 }
 
 if($stmt = mysqli_prepare($connection, "SELECT username, email, token FROM users WHERE token = ?")) {
@@ -23,21 +24,22 @@ if($stmt = mysqli_prepare($connection, "SELECT username, email, token FROM users
                 mysqli_stmt_execute($stmt);
 
                 if(mysqli_stmt_affected_rows($stmt) >= 1) {
-                    redirect('/erchat/index.php');
+                    header("Location: /erchat/index.php");
                 }
+                mysqli_stmt_close($stmt);
             }
         }
     }
 }
 ?>
 
-<div class="container">
+<div class="container cont-style">
     <div class="container">
         <div class="row">
             <div class="col-md-4 col-md-offset-4">
                 <div class="panel panel-default">
                     <div class="panel-body">
-                        <div class="text-center">
+                        <div class="text-center text-c--style">
                             <h3><i class="fa fa-lock fa-4x"></i></h3>
                             <h2 class="text-center">بازیابی رمز عبور</h2>
                             <p>رمز جدید خود را تعیین کنید</p>
@@ -46,21 +48,21 @@ if($stmt = mysqli_prepare($connection, "SELECT username, email, token FROM users
                                 <form id="register-form" role="form" autocomplete="off" class="form" method="post">
 
                                     <div class="form-group">
-                                        <div class="input-group">
+                                        <div class="input-group ig-style">
                                             <span class="input-group-addon"><i class="glyphicon glyphicon-user color-blue"></i></span>
                                             <input id="password" name="password" placeholder="رمز عبور" class="form-control"  type="password">
                                         </div>
                                     </div>
 
                                     <div class="form-group">
-                                        <div class="input-group">
+                                        <div class="input-group ig-style">
                                             <span class="input-group-addon"><i class="glyphicon glyphicon-ok color-blue"></i></span>
                                             <input id="confirmPassword" name="confirmPassword" placeholder="تکرار رمز" class="form-control"  type="password">
                                         </div>
                                     </div>
 
                                     <div class="form-group">
-                                        <input name="resetPassword" class="btn btn-lg btn-primary btn-block" value="انجام شد" type="submit">
+                                        <input name="resetPassword" class="btn btn-lg btn-primary btn-block reset-pass--btn" value="انجام شد" type="submit">
                                     </div>
 
                                     <input type="hidden" class="hide" name="token" id="token" value="">
@@ -72,6 +74,5 @@ if($stmt = mysqli_prepare($connection, "SELECT username, email, token FROM users
             </div>
         </div>
     </div>
-    <hr>
     <?php include "vendors/footer.php"; ?>
 </div>
